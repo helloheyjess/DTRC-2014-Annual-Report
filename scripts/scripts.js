7b93d30c18e50e33eb3f0415f73b010a91dc160f
helloheyjess <jessicamayblack@gmail.com>
2015-04-13 16:25:58 -0400

    DTRC 2014 Annual Report
    Final project for HackerYou’s part-time HTML/CSS course.  Annual Report
    for the Dancer Transition Resource Centre.

---
+++ scripts/scripts.js
@@ -0,0 +1,102 @@
+//Initiate WOW plugin for animations on scroll
+new WOW().init();
+
+// Initiate anyslider
+$('.slider').anyslider({
+});
+
+// jQuery
+$(function(){
+	//Fittext for Responsive Headlines
+	$('h2.responsiveHeadline').fitText(0.75);
+	$('h3.responsiveHeadline').fitText(1.3, {maxFontSize: '50px'});
+
+	//Menu
+	$('.menuBars').on('click', function() {
+		$('.nav').fadeIn();
+	});
+	$('.nav a').on('click', function() {
+		$('.nav').fadeOut();
+	});
+
+	//Open video overlay and play video when play button is clicked
+	$('.videoWrapper').on('click', function() {
+		$('.videoOverlay').fadeIn();
+		$('video#overlayVid')[0].play();
+	});
+
+	// Open full site credits
+	$('.clickCredits').on('click', function() {
+		$('.fullCredits').fadeIn();
+	});
+
+	// Closing Overlays
+	$('.close').on('click', function() {
+		// Menu
+		$('.nav').fadeOut();
+		//Video (stop playing)
+		$('.videoOverlay').fadeOut();
+		$('video#overlayVid')[0].pause();
+		// Credits
+		$('.fullCredits').fadeOut();
+	});
+
+	// Gallery
+	var $gallery = $('.gallery').flickity({
+		//options
+		cellSelector: 'img',
+		imagesLoaded: true,
+		percentPosition: false,
+		wrapAround: true,
+		freeScroll: true,
+		autoPlay: false,
+		pageDots: false
+	});
+	//Gallery Captions
+	var $otmcaption = $('.members .caption');
+	var $caption = $('.supporters .caption');
+	var flkty = $gallery.data('flickity');
+
+	$gallery.on('cellSelect', function() {
+		$otmcaption.text(flkty.selectedElement.alt)
+	});
+
+	$gallery.on('cellSelect', function() {
+		$donorcaption.text(flkty.selectedElement.alt)
+	});
+
+	// Smooth Scroll
+	$('a').smoothScroll({
+		speed: 800
+	});
+
+	// Member Spotlight Interactivity (responsive)
+	$(window).on("load resize", function() {
+	    var width = $(window).width();
+	    // Test if window is more than 940px
+	    if (width > 940) {
+	        // Toggle feature stories below images
+	        $('.leftFeature').on('click', function() {
+	        	$('#right').slideUp();
+	        	$('#left').stop().slideToggle();
+	        });
+
+	        $('.rightFeature').on('click', function() {
+	        	$('#left').slideUp();
+	        	$('#right').stop().slideToggle();
+	        });
+	    };
+
+	    //Test if window is less than 940px
+	    if (width < 940) {
+	    	//Move left feature story below image and display block
+	    	$('#left').insertAfter('.leftFeature').css('display', 'block');
+	    	$('#right').css('display', 'block');
+	    } else {
+	    	//Remove left feature story
+	    	$('#left, #right').css('display', 'none');
+	    };
+	});
+});
+
+
