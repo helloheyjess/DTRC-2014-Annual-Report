9791285990e80289f53f1fdf1399221b5112f8e5
helloheyjess <jessicamayblack@gmail.com>
2015-05-07 10:36:37 -0400

    Updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Dancer Transition Resource Centre - 2014 Annual Report
-I created this interactive Annual Report as my final project for HackerYou's Part-Time HTML/CSS course.  Designed in HTML5 / CSS3 / jQuery.
+I created this interactive Annual Report as my final project for HackerYou's Part-Time HTML/CSS course. Designed in HTML5 / CSS3 / jQuery and meant to be an interactive alternative to the printed report.
 
-Played around with navigation menu overlay, css columns, css animations, flexbox, HTML5 video and media queries.  Added interactivity with jQuery and plain old JavaScript.  I also delved into some Sass mixins and functions.  I'm looking forward to building on these skills and forever learning to write semantic, clean, efficient code! 
+Played around with navigation menu overlay, css columns, css animations, flexbox, HTML5 video and media queries. Added interactivity with jQuery. I also delved into some Sass mixins and functions. I'm looking forward to building on these skills and forever learning to write semantic, clean, efficient code!
--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,12 +12,14 @@ $(function(){
 	$('h3.responsiveHeadline').fitText(1.3, {maxFontSize: '50px'});
 
 	//Menu
-	$('.menuBars').on('click', function() {
-		$('.nav').fadeIn();
-	});
-	$('.nav a').on('click', function() {
-		$('.nav').fadeOut();
-	});
+		//Open Menu
+		$('.menuBars').on('click', function() {
+			$('.nav').fadeIn();
+		});
+		//Close menu when links are clicked
+		$('.nav a').on('click', function() {
+			$('.nav').fadeOut();
+		});
 
 	//Open video overlay and play video when play button is clicked
 	$('.videoWrapper').on('click', function() {
@@ -32,52 +34,53 @@ $(function(){
 
 	// Closing Overlays
 	$('.close').on('click', function() {
-		// Menu
+		// Close Menu
 		$('.nav').fadeOut();
-		//Video (stop playing)
+		// Close Video Overlay (and stop playing)
 		$('.videoOverlay').fadeOut();
 		$('video#overlayVid')[0].pause();
-		// Credits
+		// Close Site Credits
 		$('.fullCredits').fadeOut();
 	});
 
-	// OTM Gallery
-	var $otmgallery = $('.gallery').flickity({
-		//options
-		cellSelector: 'img',
-		imagesLoaded: true,
-		percentPosition: false,
-		wrapAround: true,
-		freeScroll: true,
-		autoPlay: false,
-		pageDots: false
-	});
-	//OTM Gallery Captions
-	var $otmcaption = $('.members .caption');
-	var flkty = $otmgallery.data('flickity');
+	// Flickity Galleries
+		// OTM Gallery
+		var $otmgallery = $('.gallery').flickity({
+			//options
+			cellSelector: 'img',
+			imagesLoaded: true,
+			percentPosition: false,
+			wrapAround: true,
+			freeScroll: true,
+			autoPlay: false,
+			pageDots: false
+		});
+		//OTM Gallery Captions
+		var $otmcaption = $('.members .caption');
+		var flkty = $otmgallery.data('flickity');
 
-	$otmgallery.on('cellSelect', function() {
-		$otmcaption.text(flkty.selectedElement.alt)
-	});
+		$otmgallery.on('cellSelect', function() {
+			$otmcaption.text(flkty.selectedElement.alt)
+		});
 
-	//Donor Event Gallery
-	var $donorgallery = $('.donorGallery').flickity({
-		//options
-		cellSelector: 'img',
-		imagesLoaded: true,
-		percentPosition: false,
-		wrapAround: true,
-		freeScroll: true,
-		autoPlay: 5000,
-		pageDots: false
-	});
-	// Donor Gallery Captions
-	var $donorcaption = $('.supporters .caption');
-	var flkty1 = $donorgallery.data('flickity');
+		//Donor Event Gallery
+		var $donorgallery = $('.donorGallery').flickity({
+			//options
+			cellSelector: 'img',
+			imagesLoaded: true,
+			percentPosition: false,
+			wrapAround: true,
+			freeScroll: true,
+			autoPlay: 5000,
+			pageDots: false
+		});
+		// Donor Gallery Captions
+		var $donorcaption = $('.supporters .caption');
+		var flkty1 = $donorgallery.data('flickity');
 
-	$donorgallery.on('cellSelect', function() {
-		$donorcaption.text(flkty1.selectedElement.alt)
-	});
+		$donorgallery.on('cellSelect', function() {
+			$donorcaption.text(flkty1.selectedElement.alt)
+		});
 
 	// Smooth Scroll
 	$('a').smoothScroll({
@@ -90,11 +93,12 @@ $(function(){
 	    // Test if window is more than 940px
 	    if (width > 940) {
 	        // Toggle feature stories below images
+	        //Left
 	        $('.leftFeature').on('click', function() {
 	        	$('#right').slideUp();
 	        	$('#left').stop().slideToggle();
 	        });
-
+	        //Right
 	        $('.rightFeature').on('click', function() {
 	        	$('#left').slideUp();
 	        	$('#right').stop().slideToggle();
@@ -103,7 +107,7 @@ $(function(){
 
 	    //Test if window is less than 940px
 	    if (width < 940) {
-	    	//Move left feature story below image and display block
+	    	//Move left feature story below image and display block both features
 	    	$('#left').insertAfter('.leftFeature').css('display', 'block');
 	    	$('#right').css('display', 'block');
 	    } else {
